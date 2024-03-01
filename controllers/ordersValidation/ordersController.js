01b2503afb3cd6fbd17cde3b291e695922327235
Abhishek7356 <ksabhishek32@gmail.com>
2024-03-01 17:10:08 +0530

    updated order functionalities

--- controllers/ordersValidation/ordersController.js
+++ controllers/ordersValidation/ordersController.js
@@ -2,47 +2,48 @@ import Order from "../../models/ordersModel.js";
 
 
 //checkout details
-export const orderDetails=async(req,res)=>{
-    try{
-        const newOrder=new Order({...req.body,userId:req.payload})
+export const orderDetails = async (req, res) => {
+    try {
+        const newOrder = new Order({ ...req.body, userId: req.payload })
         await newOrder.save()
-        res.status(200).json(newOrder)
-      }
-       
-    catch(err){
-        res.status(401).json({error:err,message:'Order checkout failed'})
+        const newOrderAllDetails = await Order.findById(newOrder._id).populate('userId')
+        res.status(200).json(newOrderAllDetails)
+    }
+
+    catch (err) {
+        res.status(401).json({ error: err, message: 'Order checkout failed' })
     }
 }
 
 //user order details
-export const userOrder=async (req,res)=>{
-    const userId=req.payload
-    try{
-        const userOrderDetails=await Order.find({userId})
-        if(userOrderDetails){
+export const userOrder = async (req, res) => {
+    const userId = req.payload
+    try {
+        const userOrderDetails = await Order.find({ userId }).populate('userId')
+        if (userOrderDetails.length > 0) {
             res.status(200).json(userOrderDetails)
         }
-        else{
+        else {
             res.status(404).json("No orders yet")
         }
     }
-    catch(err){
-        res.status(401).json({error:err,message:'User order details access failed'})
+    catch (err) {
+        res.status(401).json({ error: err, message: 'User order details access failed' })
     }
 }
 
 //all orders (admin)
-export const allOrders=async (req,res)=>{
-    try{
-        const orders=await Order.find()
-        if(orders){
+export const allOrders = async (req, res) => {
+    try {
+        const orders = await Order.find()
+        if (orders) {
             res.status(200).json(orders)
         }
-        else{
+        else {
             res.status(404).json('Orders empty')
         }
     }
-    catch(err){
-        res.status(401).json({error:err,message:'Orders details access failed'})
+    catch (err) {
+        res.status(401).json({ error: err, message: 'Orders details access failed' })
     }
 } 
--- models/ordersModel.js
+++ models/ordersModel.js
@@ -1,53 +1,49 @@
 
 import mongoose from "mongoose";
 
-const orderSchema=new mongoose.Schema({
-    userId:{
-        type:String,
-        required:true
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+        required: true
     },
-    products:{
-    type:Array,
-    required:true
+    products: {
+        type: Array,
+        required: true
     },
-    address:{
-        type:String,
-        required:true
+    address: {
+        type: String,
+        required: true
     },
-    zipCode:{
-        type:Number,
-        required:true
+    zipCode: {
+        type: Number,
+        required: true
     },
-    city:{
-        type:String,
-        required:true
+    city: {
+        type: String,
+        required: true
     },
-    country:{
-        type:String,
-        required:true
+    country: {
+        type: String,
+        required: true
     },
-    orderStatus:{
-        type:String,
-        default:'Pending'
+    orderStatus: {
+        type: String,
+        default: 'ordered'
     },
-    totalPrice:{
-        type:Number,
-        required:true
+    totalPrice: {
+        type: Number,
+        required: true
     },
-    shippingMethod:{
-        type:String,
-        required:true
+    shippingMethod: {
+        type: String,
+        required: true
+    },
+    dateOrdered: {
+        type: Date,
+        default: Date.now,
     }
-    ,
-    appliedCoupon:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'coupons',
-    },
-    dateOrdered:{
-        type:Date,
-        default:Date.now,
-    }
-},{timestamps:true})
+}, { timestamps: true })
 
-const Order=mongoose.model('Orders',orderSchema)
+const Order = mongoose.model('Orders', orderSchema)
 export default Order;
