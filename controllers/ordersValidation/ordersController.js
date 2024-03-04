73b4a8945adb7e74bb1c8e21e1179a88d032a481
Abhishek7356 <ksabhishek32@gmail.com>
2024-03-04 10:45:11 +0530

    created new api to delete order

--- controllers/ordersValidation/ordersController.js
+++ controllers/ordersValidation/ordersController.js
@@ -1,13 +1,29 @@
 import Order from "../../models/ordersModel.js";
+import mongoose from "mongoose";
 
 
 //checkout details
 export const orderDetails = async (req, res) => {
+    const userId = new mongoose.Types.ObjectId(req.payload);
     try {
         const newOrder = new Order({ ...req.body, userId: req.payload })
         await newOrder.save()
-        const newOrderAllDetails = await Order.findById(newOrder._id).populate('userId')
-        res.status(200).json(newOrderAllDetails)
+        const AllOrdersWithNewOne = await Order.aggregate([
+            {
+                $match: {
+                    userId: userId
+                }
+            },
+            {
+                $unwind: "$products"
+            },
+            {
+                $sort: {
+                    createdAt: -1
+                }
+            }
+        ])
+        res.status(200).json(AllOrdersWithNewOne)
     }
 
     catch (err) {
@@ -17,15 +33,25 @@ export const orderDetails = async (req, res) => {
 
 //user order details
 export const userOrder = async (req, res) => {
-    const userId = req.payload
+    const userId = new mongoose.Types.ObjectId(req.payload);
+    console.log(userId)
     try {
-        const userOrderDetails = await Order.find({ userId }).populate('userId')
-        if (userOrderDetails.length > 0) {
-            res.status(200).json(userOrderDetails)
-        }
-        else {
-            res.status(404).json("No orders yet")
-        }
+        const userOrderDetails = await Order.aggregate([
+            {
+                $match: {
+                    userId: userId
+                }
+            },
+            {
+                $unwind: "$products"
+            },
+            {
+                $sort: {
+                    createdAt: -1
+                }
+            }
+        ])
+        res.status(200).json(userOrderDetails)
     }
     catch (err) {
         res.status(401).json({ error: err, message: 'User order details access failed' })
@@ -45,5 +71,16 @@ export const allOrders = async (req, res) => {
     }
     catch (err) {
         res.status(401).json({ error: err, message: 'Orders details access failed' })
+    }
+}
+
+//delete order
+export const deleteOrder = async (req, res) => {
+    try {
+        await Order.findByIdAndDelete(req.params.id)
+        res.status(200).json({ message: "Order deleted successfully" })
+    }
+    catch (err) {
+        res.status(401).json({ error: err, message: 'Orders details delete failed' })
     }
 } 
